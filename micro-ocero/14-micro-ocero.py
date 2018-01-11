8866d31878778fe6bda10c394db2d13b46f40257
gimpei kobayashi <gim.kobayashi@gmail.com>
2018-01-11 19:14:34 +0900

    a

--- micro-ocero/14-micro-ocero.py
+++ micro-ocero/14-micro-ocero.py
@@ -11,13 +11,17 @@ from keras.layers.wrappers import TimeDistributed as TD
 from keras.engine.topology import Layer
 from keras import backend as K
 from keras.layers.merge import Concatenate
+from pprint import pprint
 import numpy as np
 import random
 import sys
 import glob
 import pickle
 import re
+import json
+import ocero_detection
 
+# white, 1
 inputs_1_1  = Input( shape=(4,4) ) 
 x           = Dense(4000, activation='relu')( inputs_1_1 )
 x           = Flatten()(x)
@@ -27,14 +31,50 @@ x           = Reshape((4,4))(x)
 player1     = Model( inputs_1_1, x )
 player1.compile( optimizer=Adam(), loss='mse', metrics=['accuracy'] ) 
 
+# black, -1
 inputs_2_1  = Input( shape=(4,4) ) 
 x           = Dense(4000, activation='relu')( inputs_2_1 )
 x           = Flatten()(x)
 x           = Dense(4000, activation='relu')( x )
 x           = Dense(16, activation='linear')( x )
 x           = Reshape((4,4))(x)
-#print(x.shape)
 player2     = Model( inputs_2_1, x )
 player2.compile( optimizer=Adam(), loss='mse', metrics=['accuracy'] )
 
 #optimizers = [SGD(), Adam(), RMSprop()]
+X = [
+  [0, 0, 0, 0],
+  [0, 1, -1, 0],
+  [0, -1, 1, 0],
+  [0, 0, 0, 0],
+]
+y = player1.predict( np.array([X]) )
+pprint( y.tolist()[0] )
+Xstatus = {index:x for index, x in enumerate(sum(X, []))}
+point =  list(filter(lambda x: Xstatus.get(x[0]) == 0, [(index,prob) for index, prob in sorted(enumerate(sum(y.tolist()[0], [])), key=lambda x:x[1]*-1)] ) )[0]
+p, _ = point
+y = p//4
+x = p%4 
+X[y][x] = 1
+# eval and update playter2
+X = ocero_detection.update(X, y, x, type=1)
+[print(x) for x in X]
+print('-'*10)
+# この時の、1, -1の数
+size_1_1 = len(list(filter(lambda x:x==1, sum(X, []))))
+size_1_2 = len(list(filter(lambda x:x==-1, sum(X, []))))
+
+y = player2.predict( np.array([X]) )
+Xstatus = {index:x for index, x in enumerate(sum(X, []))}
+point =  list(filter(lambda x: Xstatus.get(x[0]) == 0, [(index,prob) for index, prob in sorted(enumerate(sum(y.tolist()[0], [])), key=lambda x:x[1]*-1)] ) )[0]
+p, _ = point
+y = p//4
+x = p%4 
+X[y][x] = -1
+# eval and update player1
+X = ocero_detection.update(X, y, x, type=-1)
+[print(x) for x in X]
+print('-'*10)
+
+size_2_1 = len(list(filter(lambda x:x==1, sum(X, []))))
+size_2_2 = len(list(filter(lambda x:x==-1, sum(X, []))))
---
+++ micro-ocero/ocero_detection.py
@@ -0,0 +1,66 @@
+import numpy as np
+def update(X, y, x, type=1):
+  W,H = np.array(X).shape 
+  print(W,H)
+  print(X)
+
+  # scan to right
+  buff = []
+  for i in range(x+1, W):
+    scan = X[y][i]
+    if scan == 0:
+      break
+    if scan == type:
+      # 反転させる
+      for b in buff:
+        y, i = b
+        X[y][i] = type
+      break
+    buff.append( (y,i) )
+    print(scan)
+  
+  # scan to left
+  buff = []
+  for i in reversed(range(0, x)):
+    scan = X[y][i]
+    if scan == 0:
+      break
+    if scan == type:
+      # 反転させる
+      for b in buff:
+        y, i = b
+        X[y][i] = type
+      break
+    buff.append( (y,i) )
+    print(scan)
+ 
+  # scan to duttom
+  buff = []
+  for i in range(y+1, H):
+    scan = X[i][x]
+    if scan == 0:
+      break
+    if scan == type:
+      # 反転させる
+      for b in buff:
+        i, x = b
+        X[i][x] = type
+      break
+    buff.append( (i, x) )
+    print(scan)
+  
+  # scan to top
+  buff = []
+  for i in reversed(range(0, y)):
+    scan = X[i][x]
+    if scan == 0:
+      break
+    if scan == type:
+      # 反転させる
+      for b in buff:
+        i, x = b
+        X[i][x] = type
+      break
+    buff.append( (i, x) )
+    print(scan)
+  return X
